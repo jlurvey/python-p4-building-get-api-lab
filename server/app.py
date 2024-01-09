f4d79b7d92795710c748848f465dcd41d00c297a
Joseph Lurvey <joeylurvey@gmail.com>
2024-01-08 20:54:12 -0500

    Complete lab with all tests passing

--- server/app.py
+++ server/app.py
@@ -20,19 +20,33 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries_list = [bakery.to_dict() for bakery in Bakery.query.all()]
+    return jsonify(bakeries_list)
+
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    
+    baked_goods = [goods.to_dict() for goods in bakery.baked_goods]
+    
+    bakery_data = bakery.to_dict()
+    
+    bakery_data['baked_goods'] = baked_goods
+    
+    return jsonify(bakery_data)
+
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    sorted_baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    sorted_baked_goods_list = [goods.to_dict() for goods in sorted_baked_goods]
+    return jsonify(sorted_baked_goods_list)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive_baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first().to_dict()
+    return jsonify(most_expensive_baked_good)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
